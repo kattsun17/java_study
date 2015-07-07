0f16867084ba315242f824c7c5435045719bd98d
Gakusei Katsumura <Gakusei@Gakusei.local>
2015-07-07 14:43:29 +0900

    (kattsun:root) READMEに追記

---
+++ 2015.07.07/JSample1_5.java
@@ -0,0 +1,13 @@
+class JSample1_5 {
+    public static void main(String args[]) {
+        char c;
+        int num;
+
+        num = 20;
+
+        c = num > 0 ? '正' : '負';
+        System.out.print(num + "は");
+        System.out.println(c);
+
+    }
+}
