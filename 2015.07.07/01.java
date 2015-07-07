521c5e44316f6eee94b61caf6b70d942bc9d902b
Gakusei Katsumura <Gakusei@Gakusei.local>
2015-07-07 10:11:51 +0900

    (kattsun:root) READMEを追記

---
+++ 2015.07.07/01.java
@@ -0,0 +1,8 @@
+class Robot {
+    void hello () {
+        System.out.println("こんにちは");
+    }
+    void bye () {
+        System.out.println("さようなら");
+    }
+}
---
+++ 2015.07.07/02.java
@@ -0,0 +1,12 @@
+class Counter{
+    int num;
+    void init(){
+        num = 0;
+    }
+    void count(){
+        num = num + 1;
+    }
+    void print(){
+        System.out.println(num);  
+    } 
+}
