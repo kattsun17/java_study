9082c8ab035dadb64dbdee591e44be45cde56cce
Gakusei Katsumura <Gakusei@Gakusei.local>
2015-07-07 13:57:06 +0900

    (kattsun:root) READMEを追記

---
+++ 2015.07.07/JSample1_3.java
@@ -0,0 +1,11 @@
+class JSample1_3 {
+    public static void main (String args[]) {
+        int num = 1;
+
+        num = num * 2;
+        System.out.println(num);
+
+        num = num * 2;
+        System.out.println(num);
+    }
+}
---
+++ 2015.07.07/JSample1_4.java
@@ -0,0 +1,12 @@
+class JSample1_4 {
+    public static void main(String args[]) {
+        int sansu;
+        int rika;
+
+        sansu = 90;
+        rika = 20;
+
+        System.out.println("算数の点数は" + sansu);
+        System.out.println("理科の点数は" + rika);
+    }
+}
